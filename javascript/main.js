3314dbe5f5abb417fa5ab81c0aa81f3342ddec0d
Lucas Antonio <lam_p10@hotmail.com>
2024-01-26 19:01:06 -0300

    upadte git push

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,11 @@
 var box_1 = document.getElementById('box_1')
+var cont_box1 = document.getElementById('content-box1')
 function html(){
-    box_1.innerHTML = 'HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML. HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo.'
+    
+    
+    
 }
-
+    
 function htmlDefault(){
     box_1.setAttribute("src","img/html-5.png")
 }
